ef12c79dfbba0037ee85631127508dbc6b961c64
BZoennchen <benedikt.zoennchen@web.de>
2024-06-23 00:19:12 +0200

    uncomment example for now.

---
+++ examples/mesh.rs
@@ -0,0 +1,9 @@
+use distmesh::Point;
+use distmesh::geometry::equiliteral_triangle;
+use distmesh::prelude::*;
+
+pub fn main() {
+  /*let (p1, p2, p3) = equiliteral_triangle(100.0);
+  let mesh = Mesh::triangle(p1, p2, p3);
+  println!("{}", distmesh::mesh::tikz::to_tikz_string(&mesh));*/
+}
